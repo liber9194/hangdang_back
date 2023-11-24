e7e58f99f649352f9dba230c5c9cfc52e430ed47
seeyoungryu <seeyoungryu@gmail.com>
2023-11-24 19:26:34 +0900

    webconfig, application.properties 수정 버전 (rds, localhost:3000)

--- src/main/java/com/example/hangdang/config/WebConfig.java
+++ src/main/java/com/example/hangdang/config/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins()
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
